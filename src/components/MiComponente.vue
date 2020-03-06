9ca834ec197d95db4ac94470db093f459c95ea7d
Cristian <ecristian963@gmail.com>
2020-03-06 16:14:49 -0500

    archivos iniciales
    Componente "MiComponente" Agregado

---
+++ src/components/MiComponente.vue
@@ -0,0 +1,19 @@
+<template>
+    <H1>Compra y venta de partes</H1>
+
+    
+    
+    
+</template>
+
+<script>
+export default {
+    name: "MiComponente"
+}
+</script>
+
+<style scoped>
+h1 {
+    margin: 50px 0 0;
+}
+</style>
