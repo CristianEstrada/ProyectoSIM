e82dc29419f5e9b58fdc92f63834a703c3ebf8a6
Cristian <ecristian963@gmail.com>
2020-03-06 18:47:38 -0500

    Update MiComponente.vue
    Se agrego div de prueba

--- src/components/MiComponente.vue
+++ src/components/MiComponente.vue
@@ -1,7 +1,14 @@
 <template>
-    <H1>Compra y venta de partes</H1>
-
     
+
+    <div class="hello">
+        <H1>Compra y venta de partes</H1>
+
+        <label for="nombre">Primer nombre:</label> <br> <br>
+        <input type="text" id="nombre" name="nombre">
+
+    </div>
+
     
     
 </template>
@@ -15,5 +22,8 @@ export default {
 <style scoped>
 h1 {
     margin: 50px 0 0;
+}
+div {
+    background-color: aqua;
 }
 </style>
